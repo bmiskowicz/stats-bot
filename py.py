1ab67dbc0125bff5559cd56d8bccbe2e275a6c44
Brajan Miśkowicz <73662402+xReix@users.noreply.github.com>
2022-04-05 16:21:34 +0200

    05.04.2022 v2
    poprawione działanie list. dodane komentarze

--- py.py
+++ py.py
@@ -6,7 +6,7 @@ import os
 import re
 
 
-def getData(url):
+def getData(name, team, link, url):
     #Make a GET request to fetch the raw HTML content
     html_content = requests.get(url).text
 
@@ -21,19 +21,19 @@ def getData(url):
     a = zeros(18, float)
 
     for td in gdp_table.find_all("tr"):
-        #print(td.text)
         tekst = td.text
         tekst=tekst.split()
+        #looking for stats from whole season
         if "Łącznie" in tekst:
             for xd in tekst:
                 try:
-                    xd = xd.replace(',','.')
+                    xd = xd.replace(',','.') #changing comas to dots
                     a[i]=float(xd)
                     i=i+1
                 except ValueError:
                     pass    
-    #print(a)
 
+    #save stats to .txt file
     local_dir = os.path.dirname(__file__)
     with open(os.path.join(local_dir, "staty.txt"), "a") as file:
         data = ' '.join(str(e) for e in a)
@@ -49,40 +49,44 @@ def getPlayers():
 
     #Get the table having the div 
     gdpPlayers = soupPlayers.find("div", id="hiddenPlayersListAllBuffer")
+
+    #tables for players data
     names_table = []
     teams_table = []
     links_table = []
     positions_table = []
 
     
-    
-    names = ("\n".join([img['alt'] for img in gdpPlayers.find_all('img', alt=True)]))
-
+    #get the names and teams of players
     i=0
-    for element in names:
+    names = ("\n".join([img['alt'] for img in gdpPlayers.find_all('img', alt=True)]))
+    names = names.split('\n')
+    for line in names:
         if i%2==0:
-            names_table.append(element)
+            names_table.append(line)
         else:
-            teams_table.append(element)
+            teams_table.append(line)
         i=i+1
 
+    #get the links to player stats page
     i=0
     for link in gdpPlayers.find_all('a'):
+        #catch link every two link, cause it gets all links twice
         if i%2==0:
             link = link.get('href')
+            #change link so it can be user directly
             link = link[8:]
             link = "https://www.plusliga.pl/statsPlayers" + link
             links_table.append(link)
-            print(link)
         i=i+1
     
+    #getting the positions of players
     for data in gdpPlayers.findAll("div", {"class": "playerposition"}):
         positions_table.append(data.text.strip())
         
-        
+
+    
 
 
 
 getPlayers()
-
-getData("https://www.plusliga.pl/statsPlayers/id/304.html")
