ffe0b10f2f56c416cba12807d09758001218763d
Brajan Miśkowicz <73662402+xReix@users.noreply.github.com>
2022-04-05 20:21:34 +0200

    05.04.2022 v3
    dodane przesyłanie danych do sheetów w excelu, dodana efektywnośc ataku

--- py.py
+++ py.py
@@ -1,12 +1,28 @@
 # importing the libraries
+from glob import glob
 from numpy import zeros
 from bs4 import BeautifulSoup
 import requests
+import win32com.client as win32
 import os
-import re
+import xlwings as xw
+
+zawodnicy = 2
+srodkowi = 2
+atakujacy = 2
+libero = 2
+przyjmujacy = 2
+rozgrywajacy = 2
 
 
-def getData(name, team, link, url):
+def getData(name, team, url, position):
+    global zawodnicy
+    global srodkowi
+    global atakujacy
+    global libero
+    global przyjmujacy
+    global rozgrywajacy
+
     #Make a GET request to fetch the raw HTML content
     html_content = requests.get(url).text
 
@@ -33,11 +49,68 @@ def getData(name, team, link, url):
                 except ValueError:
                     pass    
 
+    
     #save stats to .txt file
-    local_dir = os.path.dirname(__file__)
-    with open(os.path.join(local_dir, "staty.txt"), "a") as file:
-        data = ' '.join(str(e) for e in a)
-        file.write(f'{data}\n') 
+    if a[0] != 0:
+        
+        
+        # Open existing Workbooks
+        wb = xw.Book('zawodnicy.xlsx')  
+        sheet1 = wb.sheets['zawodnicy']
+        sheet2 = wb.sheets['atakujacy']
+        sheet3 = wb.sheets['rozgrywajacy']
+        sheet4 = wb.sheets['przyjmujacy']
+        sheet5 = wb.sheets['srodkowi']
+        sheet6 = wb.sheets['libero']
+
+        if zawodnicy == 2:
+            sheet1.range('A1').value = ['Nazwisko', 'Klub', 'Pozycja', 'Liczba setów', 'Punkty', 'Suma zagrywek', 'Asy serwisowe', 'Błędy na zagrywce', 'Asy na set', 'Suma przyjęć', 'Błędy w przyjęciu', 'Negatywne przyjęcia', 'Przyjęcia', '% przyjęcia', 'Suma ataków', 'Błędy w ataku', 'Zablokowane ataki', 'Skończone ataki', '% Skuteczności ataku', '% efektywności ataku', 'Bloki', 'Bloki na set']
+            sheet2.range('A1').value = ['Nazwisko', 'Klub', 'Pozycja', 'Liczba setów', 'Punkty', 'Suma zagrywek', 'Asy serwisowe', 'Błędy na zagrywce', 'Asy na set', 'Suma przyjęć', 'Błędy w przyjęciu', 'Negatywne przyjęcia', 'Przyjęcia', '% przyjęcia', 'Suma ataków', 'Błędy w ataku', 'Zablokowane ataki', 'Skończone ataki', '% Skuteczności ataku', '% efektywności ataku', 'Bloki', 'Bloki na set']
+            sheet3.range('A1').value = ['Nazwisko', 'Klub', 'Pozycja', 'Liczba setów', 'Punkty', 'Suma zagrywek', 'Asy serwisowe', 'Błędy na zagrywce', 'Asy na set', 'Suma przyjęć', 'Błędy w przyjęciu', 'Negatywne przyjęcia', 'Przyjęcia', '% przyjęcia', 'Suma ataków', 'Błędy w ataku', 'Zablokowane ataki', 'Skończone ataki', '% Skuteczności ataku', '% efektywności ataku', 'Bloki', 'Bloki na set']
+            sheet4.range('A1').value = ['Nazwisko', 'Klub', 'Pozycja', 'Liczba setów', 'Punkty', 'Suma zagrywek', 'Asy serwisowe', 'Błędy na zagrywce', 'Asy na set', 'Suma przyjęć', 'Błędy w przyjęciu', 'Negatywne przyjęcia', 'Przyjęcia', '% przyjęcia', 'Suma ataków', 'Błędy w ataku', 'Zablokowane ataki', 'Skończone ataki', '% Skuteczności ataku', '% efektywności ataku', 'Bloki', 'Bloki na set']
+            sheet5.range('A1').value = ['Nazwisko', 'Klub', 'Pozycja', 'Liczba setów', 'Punkty', 'Suma zagrywek', 'Asy serwisowe', 'Błędy na zagrywce', 'Asy na set', 'Suma przyjęć', 'Błędy w przyjęciu', 'Negatywne przyjęcia', 'Przyjęcia', '% przyjęcia', 'Suma ataków', 'Błędy w ataku', 'Zablokowane ataki', 'Skończone ataki', '% Skuteczności ataku', '% efektywności ataku', 'Bloki', 'Bloki na set']
+            sheet6.range('A1').value = ['Nazwisko', 'Klub', 'Pozycja', 'Liczba setów', 'Punkty', 'Suma zagrywek', 'Asy serwisowe', 'Błędy na zagrywce', 'Asy na set', 'Suma przyjęć', 'Błędy w przyjęciu', 'Negatywne przyjęcia', 'Przyjęcia', '% przyjęcia', 'Suma ataków', 'Błędy w ataku', 'Zablokowane ataki', 'Skończone ataki', '% Skuteczności ataku', '% efektywności ataku', 'Bloki', 'Bloki na set']
+
+
+        data = ','.join(str(e) for e in a)
+        data = name + ',' + team + ',' + position + ',' + data
+        data = data.split(',')
+
+        if float(data[14]) != 0:
+            skut = ((float(data[17])-float(data[16])-float(data[15]))*100)/float(data[14])
+        else:
+            skut = 0
+
+        data.insert(19, str('%.2f'%skut))
+        pos = 'A' + str(zawodnicy)
+        sheet1.range(pos).value = data
+        zawodnicy = zawodnicy + 1
+
+            
+        if position == "atakujący":
+            pos = 'A' + str(atakujacy)
+            sheet2.range(pos).value = data
+            atakujacy = atakujacy + 1
+                    
+        if position == "rozgrywający":
+            pos = 'A' + str(rozgrywajacy)
+            sheet3.range(pos).value = data
+            rozgrywajacy = rozgrywajacy + 1
+                    
+        if position == "przyjmujący":
+            pos = 'A' + str(przyjmujacy)
+            sheet4.range(pos).value = data
+            przyjmujacy = przyjmujacy + 1
+                    
+        if position == "środkowy":
+            pos = 'A' + str(srodkowi)
+            sheet5.range(pos).value = data
+            srodkowi = srodkowi + 1
+                    
+        if position == "libero":
+            pos = 'A' + str(libero)
+            sheet6.range(pos).value = data
+            libero = libero + 1
 
 
 def getPlayers():
@@ -50,7 +123,7 @@ def getPlayers():
     #Get the table having the div 
     gdpPlayers = soupPlayers.find("div", id="hiddenPlayersListAllBuffer")
 
-    #tables for players data
+    #for players data
     names_table = []
     teams_table = []
     links_table = []
@@ -84,9 +157,11 @@ def getPlayers():
     for data in gdpPlayers.findAll("div", {"class": "playerposition"}):
         positions_table.append(data.text.strip())
         
+    #get the stats of every player
+    for i in range(len(names_table)):
+        getData(names_table[i], teams_table[i], links_table[i], positions_table[i])      
+
 
     
-
-
 
 getPlayers()
